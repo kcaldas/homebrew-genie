973caad4335bc7871c18d90b2d6a25393f1002dc
Krishna Caldas <krishnacaldas@gmail.com>
2025-07-27 15:33:40 +1200

    Genie  formula v0.1.6

--- Formula/genie.rb
+++ Formula/genie.rb
@@ -6,8 +6,10 @@ class Genie < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kcaldas/genie/releases/download/v0.1.6/genie_0.1.6_darwin_arm64.tar.gz"
+      sha256 "262ff180ba5727a75515f9cc4a3779073647ffb3a3cacb6eaf7e24bbbf6d7b40"
     else
       url "https://github.com/kcaldas/genie/releases/download/v0.1.6/genie_0.1.6_darwin_amd64.tar.gz"
+      sha256 "46388295e8c7c979c199c9b7a9a27437253e35eff970e8ef07308debbdea8093"
     end
     sha256 "replace_with_actual_sha256"
   end
@@ -15,8 +17,10 @@ class Genie < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kcaldas/genie/releases/download/v0.1.6/genie_0.1.6_linux_arm64.tar.gz"
+      sha256 "fecf44f93d2d046aaacead87b4889f4ed04d4fc35c483ecf15c88fc32b350301"
     else
       url "https://github.com/kcaldas/genie/releases/download/v0.1.6/genie_0.1.6_linux_amd64.tar.gz"
+      sha256 "214ebfaee92b70342a4cdf311543ebc4cb7a343c025a07dbf7340f323f567573"
     end
     sha256 "replace_with_actual_sha256"
   end
